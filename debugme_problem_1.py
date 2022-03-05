8dc9ff5cfdfd2f77d0189f4eed6f46d720def8e3
kinikarvaidehi <KINIKARVAIDEHI@GMAIL.COM>
2022-03-05 13:52:23 +0530

    Update debugme_problem_1.py

--- debugme_problem_1.py
+++ debugme_problem_1.py
@@ -1,12 +1,6 @@
-"""
-PROBLEM 1:-
-MAX POINTS = 5
-EXPECTED OUTPUT = apple mango banana grapes kiwi
-"""
-
-fruit=("apple , mango, orange, banana, grapes")
+fruit=["apple , mango, orange, banana, grapes"]
 
 fruit.append("kiwi")
 
-for i in fruit
-    printf("i");
+for i in fruit:
+    print(i)
