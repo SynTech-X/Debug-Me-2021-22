317726670190eb9abf32a1cc2704cdba32951f14
rohu0510 <65508466+rohu0510@users.noreply.github.com>
2022-03-05 13:35:39 +0530

    Update debugme_problem_2.py

--- debugme_problem_2.py
+++ debugme_problem_2.py
@@ -15,18 +15,60 @@ EXPECTED OUTPUT
                 8 7 6 5 4 3 2 1 0 
 
 """
-N = 3;
-def rotateMatrix(matrix):
-        i = N - 1;
-        while(i >0):
-                j = N - 1;
-                while(j > 0):
-                        print(matrix[j][i], end = "\n");
-                        j = j +1;
-                print();
-                i = i +1;
 
-matrix = [[0, 1, 2, 3, 4, 5, 6, 7, 8],
+def rotateMatrix(mat):
+
+	if not len(mat):
+		return
+
+	top = 0
+	bottom = len(mat)-1
+
+	left = 0
+	right = len(mat[0])-1
+
+	while left < right and top < bottom:
+
+
+		prev = mat[top+1][left]
+
+		for i in range(left, right+1):
+			curr = mat[top][i]
+			mat[top][i] = prev
+			prev = curr
+
+		top += 1
+
+		for i in range(top, bottom+1):
+			curr = mat[i][right]
+			mat[i][right] = prev
+			prev = curr
+
+		right -= 1
+
+		for i in range(right, left-1, -1):
+			curr = mat[bottom][i]
+			mat[bottom][i] = prev
+			prev = curr
+
+		bottom -= 1
+
+		for i in range(bottom, top-1, -1):
+			curr = mat[i][left]
+			mat[i][left] = prev
+			prev = curr
+
+		left += 1
+
+	return mat
+
+def printMatrix(mat):
+	for row in mat:
+		print row
+
+
+matrix =[
+			[0, 1, 2, 3, 4, 5, 6, 7, 8],
          [0, 1, 2, 3, 4, 5, 6, 7, 8],
        [0, 1, 2, 3, 4, 5, 6, 7, 8],
        [0, 1, 2, 3, 4, 5, 6, 7, 8],
@@ -34,6 +76,9 @@ matrix = [[0, 1, 2, 3, 4, 5, 6, 7, 8],
        [0, 1, 2, 3, 4, 5, 6, 7, 8],
        [0, 1, 2, 3, 4, 5, 6, 7, 8],
        [0, 1, 2, 3, 4, 5, 6, 7, 8],
-       [0, 1, 2, 3, 4, 5, 6, 7, 8]]
+       [0, 1, 2, 3, 4, 5, 6, 7, 8]
+		]
 
-rotateMatrix(matrix);
+
+matrix = rotateMatrix(matrix)
+printMatrix(matrix)
