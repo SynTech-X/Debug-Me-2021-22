c064389a27504750f1d8ec05affd2f2b1d226b32
kinikarvaidehi <KINIKARVAIDEHI@GMAIL.COM>
2022-03-05 14:03:22 +0530

    Update debugme_problem_4.java

--- debugme_problem_4.java
+++ debugme_problem_4.java
@@ -1,7 +1,33 @@
-/*
-       Identify the problem here and debug the program.
-*/
+public class Problem4 {
 
+    class A{
+        void foo(){
+            System.out.println("Inside Class A");
+        }
+    }
+
+    class B extends A{
+
+        void foo(){
+            System.out.println("Inside Class B");
+        }
+    }
+
+    class C extends A{
+        void foo(){
+            System.out.println("Inside Class C");
+        }
+    }
+
+    class D extends C{
+        void foo(){
+            System.out.println("Inside Class D");
+        }
+    }
+
+    public static void main(String[] args) {
+
+       
 public class Problem4 {
 
     class A{
@@ -31,8 +57,7 @@ public class Problem4 {
 
     public static void main(String[] args) {
 
-        D d = new D();
+        String d = new D();
         d.foo();
     }
 }
-
