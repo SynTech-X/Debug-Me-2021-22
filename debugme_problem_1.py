97c2ec84618da49d3733d6ab9027de39f6f68f56
caesar-cipher <100844299+caesar-cipher@users.noreply.github.com>
2022-03-05 13:18:10 +0530

    Update debugme_problem_1.py

--- debugme_problem_1.py
+++ debugme_problem_1.py
@@ -4,10 +4,9 @@ MAX POINTS = 5
 EXPECTED OUTPUT = apple mango banana grapes kiwi
 """
 
-fruit=("apple , mango, orange, banana, grapes")
+fruit=["apple" , "mango", "orange", "banana", "grapes"]
 
 fruit.remove("orange")
 fruit.append("kiwi")
 
-for i in fruit
-    printf("i");
+print(" ".join(fruit))
