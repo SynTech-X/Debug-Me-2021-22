192751717c3e919fe71e5f6602499a4ca09e81ad
Amir Pathan <amir.pathan.180@gmail.com>
2022-03-05 13:20:39 +0530

    debugme_problem_1

--- debugme_problem_1.py
+++ debugme_problem_1.py
@@ -4,9 +4,9 @@ MAX POINTS = 5
 EXPECTED OUTPUT = apple mango banana grapes kiwi
 """
 
-fruit=("apple , mango, orange, banana, grapes")
-
-fruit.append("kiwi")
-
-for i in fruit
-    printf("i");
+fruit=("apple" , "mango", "orange", "banana", "grapes")
+newList = list(fruit)
+newList.append("kiwi")
+fruit = tuple(newList)
+for i in fruit:
+    print(i,  end =" ");
